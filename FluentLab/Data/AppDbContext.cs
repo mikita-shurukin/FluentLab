5342eb0949cbc9d71d9ac0db09e075266cac9090
Mikita Shurukin <mikita.shurukin@gmail.com>
2024-11-10 21:55:32 +0100

    Add project files.

---
+++ FluentLab/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FluentLab.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentLab.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ FluentLab/ViewsModels/VerifyEmailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentLab.ViewsModels
+{
+    public class VerifyEmailViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
